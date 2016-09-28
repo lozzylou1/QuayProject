b0bda599b5ec7f028a8300aec5cbae6d8fa23254
ryanbrogden <ryanbrogden3@gmail.com>
2016-09-28 15:29:58 +0100

    Getters and Setters for Supplier class

--- Quay-Gnomes/src/main/java/Supplier.java
+++ Quay-Gnomes/src/main/java/Supplier.java
@@ -4,4 +4,23 @@ public class Supplier {
 		private String supplierName;
 		private String contactNumber;
 		private int supplierID;
+		
+		public String getSupplierName() {
+			return supplierName;
+		}
+		public void setSupplierName(String supplierName) {
+			this.supplierName = supplierName;
+		}
+		public String getContactNumber() {
+			return contactNumber;
+		}
+		public void setContactNumber(String contactNumber) {
+			this.contactNumber = contactNumber;
+		}
+		public int getSupplierID() {
+			return supplierID;
+		}
+		public void setSupplierID(int supplierID) {
+			this.supplierID = supplierID;
+		}
 }
