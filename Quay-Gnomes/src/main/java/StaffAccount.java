eefae730900bc54136f777ef37fced6beab9c82e
ryanbrogden <ryanbrogden3@gmail.com>
2016-09-28 16:30:14 +0100

    Named Queries

--- Quay-Gnomes/src/main/java/StaffAccount.java
+++ Quay-Gnomes/src/main/java/StaffAccount.java
@@ -4,43 +4,45 @@ import javax.validation.constraints.*;
 @Entity
 @Table (name = "StaffAccount")
 
-@NamedQueries ({name=StaffAccount.FIND_STAFF_ACCOUNT,
-		query = "SELECT username, password "
-				+ "FROM StaffAccount "
-				+ "WHERE username = :username")})
+@NamedQueries ({
+	@NamedQuery(name = StaffAccount.FIND_STAFF_ACCOUNT,
+			query = "SELECT username, password "
+					+ "FROM StaffAccount "
+					+ "WHERE username = :username")
+})
 
 public class StaffAccount {
 
 	/** Named Query to find the staff account */
 	public static final String FIND_STAFF_ACCOUNT = "StaffAccount.findStaffAccount";
-	
+
 	@Column (name = "username", nullable = false)
 	@Null
 	private String username;
-	
+
 	@Column (name = "password", nullable = false)
 	@NotNull
 	private String password;
 
-	
+
 	/** Returns the User name for a StaffAccount*/
 	public String getUsername() 
 	{
 		return username;
 	}
-	
+
 	/** Sets the User name for the StaffAccount*/
 	public void setUsername(String username) 
 	{
 		this.username = username;
 	}
-	
+
 	/** Returns the Password for a StaffAccount*/
 	public String getPassword() 
 	{
 		return password;
 	}
-	
+
 	/** Sets the Password for the StaffAccount*/
 	public void setPassword(String password) {
 		this.password = password;
