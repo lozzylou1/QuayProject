29c13f1b99be6c7e6fc1f7fab02766a3885555b2
ryanbrogden <ryanbrogden3@gmail.com>
2016-09-30 12:40:39 +0100

    Managers and OfflineManagers

--- Quay-Gnomes/src/main/java/InitialData.java
+++ Quay-Gnomes/src/main/java/InitialData.java
@@ -1,37 +1,61 @@
 import java.util.ArrayList;
-import java.util.List;
 
 import javax.inject.Singleton;
 
 @Singleton
 public class InitialData {
 	
+	/**Array List of Suppliers*/
 	private ArrayList<Supplier> suppliers =
 			new ArrayList<Supplier>();
 	
-	/** Creates the initial offline data*/
+	/**ArrayList of StaffAccounts*/
+	private ArrayList<StaffAccount> staffAccounts =
+			new ArrayList<StaffAccount>();
+	
+	/**Creates the initial offline data*/
 	public InitialData()
 	{
 		suppliers.add(new Supplier("Super Gnome Supplies", "+447654893756", 1));
 		suppliers.add(new Supplier("Jaccuzzi 'R' Us", "+44769435851", 2));
+		
+		staffAccounts.add(new StaffAccount("Al Stock", "Password"));
 	}
 	
-	/** Returns the dummy list of suppliers */
-	public List<Supplier> getSuppliers()
+	/**Returns the dummy list of suppliers */
+	public ArrayList<Supplier> getSuppliers()
 	{
 		return suppliers;		
 	}
 	
-	/** Add a supplier to the dummy list of suppliers*/
+	/**Returns the Dummy list of StaffAccounts*/
+	public ArrayList<StaffAccount> getStaffAccounts()
+	{
+		return staffAccounts;
+	}
+	
+	/**Add a supplier to the dummy list of suppliers*/
 	public void addSupplier(Supplier supplier)
 	{
 		suppliers.add(supplier);
 	}
 	
-	/** Set the dummy list of suppliers*/
+	/**Add a StaffAccount to the dummy list of StaffAccounts*/
+	public void addStafAccount(StaffAccount staffAccount)
+	{
+		staffAccounts.add(staffAccount);
+	}
+	
+	/**Fills the dummy list of suppliers with a another list of Suppliers*/
 	public void setSuppliers(ArrayList<Supplier> suppliers)
 	{
 		this.suppliers = suppliers;
+	}
+	
+	/**Fills the dummy list of StaffAccounts with another list of StaffAccounts*/
+	public void setStaffAccounts(ArrayList<StaffAccount> staffAccounts)
+	{
+		this.staffAccounts = staffAccounts;
 	}
 
 }
