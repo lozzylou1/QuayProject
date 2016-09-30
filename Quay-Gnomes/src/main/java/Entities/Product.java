f9a4388b8bf5c961e8b3566dbe624899b814beee
hlloyd <hlloyd64@gmail.com>
2016-09-30 14:39:51 +0000

    Getters and setters added

--- Quay-Gnomes/src/main/java/Entities/Product.java
+++ Quay-Gnomes/src/main/java/Entities/Product.java
@@ -2,9 +2,11 @@ package Entities;
 import javax.persistence.*;
 
 @Entity
-@Table (name = "Products")
+@Table (name = "Product")
 
-public class Products {
+
+//Need to do foreign keys
+public class Product {
 	
 	@Id
 	@Column (name = "productID", nullable = false)
@@ -38,7 +40,7 @@ public class Products {
 	private String dimensions;
 	
 	
-	public Products(int p_productID, String p_productName, float p_price, Boolean p_discontinued, int p_stockLevel, float p_weight, String p_dimensions){
+	public Product(int p_productID, String p_productName, float p_price, Boolean p_discontinued, int p_stockLevel, float p_weight, String p_dimensions){
 		
 		this.productID = p_productID;
 		this.productName = p_productName;
@@ -48,7 +50,76 @@ public class Products {
 		this.weight = p_weight;
 		this.dimensions = p_dimensions;
 		
-		
+	}
+
+
+	public int getProductID() {
+		return productID;
+	}
+
+
+	public void setProductID(int productID) {
+		this.productID = productID;
+	}
+
+
+	public String getProductName() {
+		return productName;
+	}
+
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+
+	public float getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+
+	public Boolean getDiscontinued() {
+		return discontinued;
+	}
+
+
+	public void setDiscontinued(Boolean discontinued) {
+		this.discontinued = discontinued;
+	}
+
+
+	public int getStockLevel() {
+		return stockLevel;
+	}
+
+
+	public void setStockLevel(int stockLevel) {
+		this.stockLevel = stockLevel;
+	}
+
+
+	public float getWeight() {
+		return weight;
+	}
+
+
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+
+
+	public String getDimensions() {
+		return dimensions;
+	}
+
+
+	public void setDimensions(String dimensions) {
+		this.dimensions = dimensions;
 	}	
 	
 
