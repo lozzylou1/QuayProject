fa3e750ff746d4b02977635d41681eb4c464b29d
hlloyd <hlloyd64@gmail.com>
2016-09-30 11:08:07 +0000

    product entities added

--- Quay-Gnomes/src/main/java/Products.java
+++ Quay-Gnomes/src/main/java/Products.java
@@ -1,14 +1,54 @@
+import javax.persistence.*;
+
+@Entity
+@Table (name = "Products")
 
 public class Products {
 	
-	private float price;
-	private String productName;
-	private Boolean discontinued;
-	private int stockLevel;
+	@Id
+	@Column (name = "productID", nullable = false)
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private int productID;
-	private float productWeight;
-	private String productDimensions;
 	
+	@Column (name = "productName", nullable = false)
+	private String productName;
+	
+	
+	@Column (name = "price", nullable = false)
+	private float price;
+	
+	
+	@Column (name = "discontinued", nullable = false)
+	private Boolean discontinued;
+	
+	
+
+	@Column (name = "stockLevel", nullable = false)
+	private int stockLevel;
+	
+	
+
+	@Column (name = "productWeight", nullable = false)
+	private float weight;
+	
+	
+	
+	@Column (name = "productDimensions", nullable = false)
+	private String dimensions;
+	
+	
+	public Products(int p_productID, String p_productName, float p_price, Boolean p_discontinued, int p_stockLevel, float p_weight, String p_dimensions){
+		
+		this.productID = p_productID;
+		this.productName = p_productName;
+		this.price = p_price;
+		this.discontinued = p_discontinued;
+		this.stockLevel = p_stockLevel;
+		this.weight = p_weight;
+		this.dimensions = p_dimensions;
+		
+		
+	}	
 	
 
 }
