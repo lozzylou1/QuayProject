7fa58ca67e95cd1179c802134fdd9fa276f6248c
hlloyd <hlloyd64@gmail.com>
2016-09-30 14:29:38 +0000

    Managers added

---
+++ Quay-Gnomes/src/main/java/Managers/ProductManagerOffline.java
@@ -0,0 +1,29 @@
+package Managers;
+import java.util.ArrayList;
+import java.util.List;
+
+import Entities.Product;
+
+public interface ProductManagerOffline {
+	
+	public void persistProduct(Product p);
+	public void persistProducts(List<Product> p);
+	
+	public Product findById(int productID);
+	
+	
+	//read
+	public ArrayList<Product> getProducts();
+	
+	
+	//create
+	public void createProduct(Product p);
+	
+	//update
+	public void updateProduct(Product p);
+	
+	
+	
+	
+
+}
