9de174dc1a1fff5c7934c819f704768254007d88
jayvaghela1392 <jay.x@hotmail.co.uk>
2016-10-10 14:57:36 +0000

    forgot to commit old work

--- Quay-Gnomes/src/main/java/Entities/Inventory.java
+++ Quay-Gnomes/src/main/java/Entities/Inventory.java
@@ -1,8 +1,43 @@
 package Entities;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 public class Inventory {
+	
 
+	@Id
+	@Column (name = "productID", nullable = false)
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private int productID;
+	
+	@Column (name = "productName", nullable = false)
+	private String productName;
+	
+	
+	@Column (name = "price", nullable = false)
+	private float price;
+	
+
+	@Column (name = "stockLevel", nullable = false)
+	private int stockLevel;
+	
+	
+
+	@Column (name = "productWeight", nullable = false)
+	private float weight;
+	
+	
+	@Column (name = "productDimensions", nullable = false)
+	private String dimensions;
+	
+
+	
+	
+	
+	
 	/** Returns the Stock Level */
 	public int getStockLevel()
 	{
