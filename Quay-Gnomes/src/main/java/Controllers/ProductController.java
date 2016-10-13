545d60ecff84a527f24a75e65d633c768f0d9593
hlloyd <hlloyd64@googlemail.com>
2016-10-13 14:10:53 +0000

    Product Controller Added

---
+++ Quay-Gnomes/src/main/java/Controllers/ProductController.java
@@ -0,0 +1,34 @@
+package Controllers;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import Managers.ProductManager;
+import Entities.Product;
+
+@Named("products")
+@SessionScoped
+
+public class ProductController {
+
+	@Inject
+
+	private ProductManager prodManager;
+	private Product product;
+
+	public String view(int id) {
+		product= prodManager.findById(id);
+		return"product";
+		}
+	
+
+	public Product getProduct() {
+		return product;
+		}
+	
+	public void setProduct(Product product){
+		this.product= product;
+	}
+	
+}
