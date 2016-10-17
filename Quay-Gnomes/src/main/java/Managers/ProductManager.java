1b4f11c69e86a7adb67b60408db83d8e085b898a
JoshuaMoore1994 <joshua.m.moore1994@gmail.com>
2016-10-17 17:08:52 +0100

    resolving merge conflicts

--- Quay-Gnomes/src/main/java/Managers/ProductManager.java
+++ Quay-Gnomes/src/main/java/Managers/ProductManager.java
@@ -14,33 +14,18 @@ public interface ProductManager {
 	public Product findById(int productID);
 	
 	
-<<<<<<< HEAD
-	//read
+	/**Returns all products that match the term*/
 	public List<Product> getProducts(String term);
-=======
 	/**Returns all the products in the data store*/
 	public ArrayList<Product> findAll();
->>>>>>> e69acd9f53630f97b8161e6d0e4727a177bd8db7
 	
 	
 	/**Creates a product*/
 	public void createProduct(Product p);
 	
-<<<<<<< HEAD
 
-	
-	//update
-	public void updateProduct(Product p);
-	public Product findByName(String term);
-	public List<Product> findAll();
-
-	
-	
-=======
 	/**Updates a product in the data store*/
 	public void updateProduct(Product p);
-
->>>>>>> e69acd9f53630f97b8161e6d0e4727a177bd8db7
 	
 	public Product findByName(String term);	
 
