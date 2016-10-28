6e8f038bfa198ea6367056b681424b7967498889
hlloyd <hlloyd64@googlemail.com>
2016-10-28 14:14:06 +0000

    Basket Implementation

---
+++ Quay-Gnomes/src/main/java/Controllers/Basket.java
@@ -0,0 +1,82 @@
+package Controllers;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.DoubleStream;
+
+import javax.ejb.Stateful;
+
+import Entities.Product;
+
+@SuppressWarnings("serial")
+@Stateful
+public final class Basket implements Serializable {
+
+	private static List<Product> basketList = new ArrayList<Product>();
+
+
+	private double totalPriceOfBasket;
+
+	/**
+	 * Gets a copy of the basket list and returns it
+	 * 
+	 * @return List<Product>
+	 */
+	public List<Product> getBasketList()
+	{		
+		List<Product> copyList = new ArrayList<Product>(basketList);
+		return copyList;		
+	}
+
+	/**
+	 * Adds an Item to the basket
+	 * 
+	 * @param product
+	 */
+	public void add(Product product, int numberOfItems)
+	{	
+		for (int i = 0; i < numberOfItems; i++ )
+		{
+			basketList.add(product);
+		}
+	}
+
+	/**
+	 * Gets the total price of the basket
+	 * 
+	 * @return the totalPrice
+	 */
+	public double getTotalPrice() 
+	{
+		double[] price = new double[basketList.size()];
+		double total= 0;
+		if (price.length != 0)
+		{
+			for (int i = 0; i < basketList.size(); i++)
+			{
+				Product product = basketList.get(i);
+				price[i] = product.getPrice();
+			}
+			totalPriceOfBasket = DoubleStream.of(price).sum();
+			total = totalPriceOfBasket;
+		}
+		return   total;
+	}
+
+	public void remove(Product product)
+	{
+		for (Product item : basketList)
+		{
+			if (product.getProductName().equals(item.getProductName()))
+			{
+				basketList.remove(item);
+			}
+		}
+	}
+	
+	public void clear()
+	{
+		basketList.clear();
+	}
+}
